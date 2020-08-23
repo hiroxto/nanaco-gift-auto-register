563139f2692ab783e9e0de517cd345654dd58a75
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-08-23 16:09:35 +0900

    nanacoギフトを登録する処理をmainから分離

--- src/register/start.ts
+++ src/register/start.ts
@@ -23,6 +23,96 @@ const getNanacoGiftIds = (args: string[]) : string[] => {
   return giftCodes.split(',').map(c => c.trim()).filter(c => c.length !== 0);
 };
 
+/**
+ * nanaco ギフトを登録する
+ * @param giftId 登録するギフトID. ハイフンは不要.
+ */
+const registerNanacoGift = async (giftId: string): Promise<void> => {
+  const browser = await puppeteer.launch();
+  const firstURL = buildLoginUrl(giftId);
+  console.log(`Register ${giftId}`);
+  console.log(firstURL);
+  const page = await browser.newPage();
+
+  // ログイン処理.
+  // ログインページを開いて, nanaco カード番号とセキュリティコードを入力
+  console.log(`Go to ${firstURL}`);
+  await page.goto(firstURL, navigationOptions);
+
+  console.log('Type nanaco number');
+  await page.waitForSelector('input[name="XCID"]', waitForSelectorOptions)
+    .then(el => el.type(NANACO_NUMBER));
+  console.log('Type security code');
+  await page.waitForSelector('input[name="SECURITY_CD"]', waitForSelectorOptions)
+    .then(el => el.type(NANACO_SECURITY_CODE));
+
+  // ログイン をクリック
+  console.log('Click login');
+  await Promise.all([
+    page.waitForNavigation(navigationOptions),
+    page.waitForSelector('input[name="ACT_ACBS_do_LOGIN2"]', waitForSelectorOptions)
+      .then(el => el.click()),
+  ]);
+
+  // nanaco ギフト登録 をクリック
+  console.log('Click gift register menu');
+  await Promise.all([
+    page.waitForNavigation(navigationOptions),
+    page.waitForSelector('#memberNavi02', waitForSelectorOptions)
+      .then(el => el.click()),
+  ]);
+
+  // 登録ボタンのフォームから target と onsubmit を削除. 強制的に同じページで開く.
+  console.log('Remove form target and onsubmit');
+  await page.waitForSelector('form');
+  await page.$eval('form', el => el.removeAttribute('target'));
+  await page.$eval('form', el => el.removeAttribute('onsubmit'));
+
+  // 登録ボタン をクリック
+  // 強制的に同じページで開かれるため, ウィンドウの移動は不要
+  console.log('Click register button');
+  await Promise.all([
+    page.waitForNavigation(navigationOptions),
+    page.waitForSelector('input[type="image"]', waitForSelectorOptions)
+      .then(el => el.click()),
+  ]);
+
+  // ギフト ID 登録フォーム
+  // ログイン時に gid を指定したためギフト ID は既に入力されている.
+  // 登録ページの登録ボタンをクリック.
+  console.log('On sub window. Click register button.');
+  await Promise.all([
+    page.waitForNavigation(navigationOptions),
+    page.waitForSelector('#submit-button', waitForSelectorOptions)
+      .then(el => el.click()),
+  ]);
+
+  // 既に登録済みのギフト ID の場合, 別ページに飛ばされる
+  // #error500 の有無を確認して, #error500 が存在した場合は終了する.
+  const isErrorPage = await page.$('#error500').then(el => !!el);
+  if (isErrorPage) {
+    await page.screenshot({ path: `screenshot/error-${giftId}.png`, fullPage: true });
+    console.error(`ギフト ID ${giftId} は既に登録されています.`);
+    await page.close();
+    await browser.close();
+  } else {
+    await page.screenshot({ path: `screenshot/success00-${giftId}.png`, fullPage: true });
+    console.log(`ギフト ID ${giftId} を登録.`);
+
+    // 登録ボタンをクリック
+    await Promise.all([
+      page.waitForNavigation(navigationOptions),
+      page.waitForSelector('input[alt="登録する"]', waitForSelectorOptions)
+        .then(el => el.click()),
+    ]);
+
+    await page.screenshot({ path: `screenshot/success01-${giftId}.png`, fullPage: true });
+    console.log('登録完了');
+    await page.close();
+    await browser.close();
+  }
+};
+
 const main = async () => {
   if (process.argv.length <= 2) {
     console.error('引数に nanaco ギフト ID が渡されていません.');
@@ -31,91 +121,7 @@ const main = async () => {
   }
 
   const giftIds = getNanacoGiftIds(process.argv);
-  await Promise.all(giftIds.map(async giftId => {
-    const browser = await puppeteer.launch();
-    const firstURL = buildLoginUrl(giftId);
-    console.log(`Register ${giftId}`);
-    console.log(firstURL);
-    const page = await browser.newPage();
-
-    // ログイン処理.
-    // ログインページを開いて, nanaco カード番号とセキュリティコードを入力
-    console.log(`Go to ${firstURL}`);
-    await page.goto(firstURL, navigationOptions);
-
-    console.log('Type nanaco number');
-    await page.waitForSelector('input[name="XCID"]', waitForSelectorOptions)
-      .then(el => el.type(NANACO_NUMBER));
-    console.log('Type security code');
-    await page.waitForSelector('input[name="SECURITY_CD"]', waitForSelectorOptions)
-      .then(el => el.type(NANACO_SECURITY_CODE));
-
-    // ログイン をクリック
-    console.log('Click login');
-    await Promise.all([
-      page.waitForNavigation(navigationOptions),
-      page.waitForSelector('input[name="ACT_ACBS_do_LOGIN2"]', waitForSelectorOptions)
-        .then(el => el.click()),
-    ]);
-
-    // nanaco ギフト登録 をクリック
-    console.log('Click gift register menu');
-    await Promise.all([
-      page.waitForNavigation(navigationOptions),
-      page.waitForSelector('#memberNavi02', waitForSelectorOptions)
-        .then(el => el.click()),
-    ]);
-
-    // 登録ボタンのフォームから target と onsubmit を削除. 強制的に同じページで開く.
-    console.log('Remove form target and onsubmit');
-    await page.waitForSelector('form');
-    await page.$eval('form', el => el.removeAttribute('target'));
-    await page.$eval('form', el => el.removeAttribute('onsubmit'));
-
-    // 登録ボタン をクリック
-    // 強制的に同じページで開かれるため, ウィンドウの移動は不要
-    console.log('Click register button');
-    await Promise.all([
-      page.waitForNavigation(navigationOptions),
-      page.waitForSelector('input[type="image"]', waitForSelectorOptions)
-        .then(el => el.click()),
-    ]);
-
-    // ギフト ID 登録フォーム
-    // ログイン時に gid を指定したためギフト ID は既に入力されている.
-    // 登録ページの登録ボタンをクリック.
-    console.log('On sub window. Click register button.');
-    await Promise.all([
-      page.waitForNavigation(navigationOptions),
-      page.waitForSelector('#submit-button', waitForSelectorOptions)
-        .then(el => el.click()),
-    ]);
-
-    // 既に登録済みのギフト ID の場合, 別ページに飛ばされる
-    // #error500 の有無を確認して, #error500 が存在した場合は終了する.
-    const isErrorPage = await page.$('#error500').then(el => !!el);
-    if (isErrorPage) {
-      await page.screenshot({ path: `screenshot/error-${giftId}.png`, fullPage: true });
-      console.error(`ギフト ID ${giftId} は既に登録されています.`);
-      await page.close();
-      await browser.close();
-    } else {
-      await page.screenshot({ path: `screenshot/success00-${giftId}.png`, fullPage: true });
-      console.log(`ギフト ID ${giftId} を登録.`);
-
-      // 登録ボタンをクリック
-      await Promise.all([
-        page.waitForNavigation(navigationOptions),
-        page.waitForSelector('input[alt="登録する"]', waitForSelectorOptions)
-          .then(el => el.click()),
-      ]);
-
-      await page.screenshot({ path: `screenshot/success01-${giftId}.png`, fullPage: true });
-      console.log('登録完了');
-      await page.close();
-      await browser.close();
-    }
-  }));
+  await Promise.all(giftIds.map(async giftId => await registerNanacoGift(giftId)));
 };
 
 console.log(main());

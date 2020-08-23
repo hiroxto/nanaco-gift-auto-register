37bff440c48d9457ef945be5f121ea5d49a488ac
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-08-23 16:57:08 +0900

    Array.map() を一つにまとめた

--- src/bookmarklet/pick-gift-codes.js
+++ src/bookmarklet/pick-gift-codes.js
@@ -2,8 +2,7 @@
 
 (function () {
   const anchorList = [...document.querySelectorAll('a[href*="emServlet?gid"]')];
-  const nanacoGiftIds = anchorList.map(anchor => anchor.href)
-    .map(nanacoGiftLink => nanacoGiftLink.match(/gid=([a-zA-Z\d]{16})$/)[1]);
+  const nanacoGiftIds = anchorList.map(anchor => anchor.href.match(/gid=([a-zA-Z\d]{16})$/)[1]);
 
   console.log(nanacoGiftIds);
   prompt('yarn run start に渡す', nanacoGiftIds.join(' '));
